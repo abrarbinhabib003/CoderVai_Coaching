6439b39cf801981aff49fff87d5d1314c0571241
Abrar Bin Habib <abrar.isatwork@gmail.com>
2025-07-07 11:43:39 +0600

    Initial Setup Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Welcome to Coaching Management</h1>
+      <p className="mt-2">Your one-stop solution for managing coaching sessions.</p>
+
+
+<div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content text-center">
+    <div className="max-w-md">
+      <h1 className="text-5xl font-bold">Welcome to CCM</h1>
+      <p className="py-6">
+   Your one-stop solution for managing coaching sessions.
       </p>
-    </>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+
+    </div>
   )
 }
 
